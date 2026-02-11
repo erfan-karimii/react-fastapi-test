ef9f6e921dbba0e2b309c24873ec9e4ea73325c8
Masoud Kheradmandi <104209505+MasoudKheradmandi@users.noreply.github.com>
2026-02-11 13:40:25 +0330

    Create main.py

---
+++ backend/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from typing import List, Optional
+from pydantic import BaseModel
+
+app = FastAPI(title="Navigation API")
+
+
+class NavItem(BaseModel):
+    name: str
+    subnav: Optional[List[str]] = None
+
+
+@app.get("/navigation", response_model=List[NavItem])
+async def get_navigation():
+    return [
+        {"name": "صفحه اصلی", "subnav": None,"link":"/"},
+        {"name": "دسته بندی ها", "subnav": ["موبایل", "لبتاپ"],"link":"/about"},
+        {"name": "فروشگاه", "subnav": None,"link":None},
+        {"name": "وبلاگ", "subnav": None,"link":None},
+        {"name": "منوی ساده", "subnav": ["درباره ما", "سوالات متداول"],"link":None},
+    ]
