cb12c26cd00d36aef67fa41e27459fbb40faaaee
erfan-karimi <100835460+erfan-karimii@users.noreply.github.com>
2026-02-11 14:13:43 +0330

    fix: auto start back with python main.py

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,17 @@ async def get_navigation():
     return [
         {"name": "صفحه اصلی", "subnav": None,"link":"/"},
         {"name": "دسته بندی ها", "subnav": ["موبایل", "لبتاپ"],"link":"/about"},
-        {"name": "فروشگاه", "subnav": None,"link":None},
-        {"name": "وبلاگ", "subnav": None,"link":None},
-        {"name": "منوی ساده", "subnav": ["درباره ما", "سوالات متداول"],"link":None},
+        {"name": "فروشگاه", "subnav": None,"link":"/"},
+        {"name": "وبلاگ", "subnav": None,"link":"/"},
+        {"name": "منوی ساده", "subnav": ["درباره ما", "سوالات متداول"],"link":"/"},
     ]
+
+import uvicorn
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+        log_level="info"
+    )
