c6b6aef0d4b379a8e13c1cde91c114725cd00338
erfan-karimi <100835460+erfan-karimii@users.noreply.github.com>
2026-02-14 17:20:11 +0330

    .

--- frontend/src/components/header/Slider.tsx
+++ frontend/src/components/header/Slider.tsx
@@ -2,7 +2,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Autoplay } from "swiper/modules";
 
 import "swiper/css";
-import 'swiper/css/pagination';
+import "swiper/css/pagination";
 
 type Slide = {
   id: number;
@@ -18,7 +18,6 @@ interface ImageSliderProps {
 export default function ImageSlider({ slides }: ImageSliderProps) {
   return (
     <Swiper
-
       pagination={{
         dynamicBullets: true,
       }}
