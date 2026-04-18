3b00d8dc73fab5624aeca3b8c56e6efd1f8e4216
erfan-karimi <100835460+erfan-karimii@users.noreply.github.com>
2026-04-18 17:18:53 +0200

    .

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,15 +3,13 @@ import AmazingSlider from "../components/slider/AmazingSlider"
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import { useAuth } from "../context/AuthContext";
 
 
 export default function Index() {
-  const { user } = useAuth()
 
   return (
     <>
-      { user ? <AmazingSlider /> : "please login" }
+      <AmazingSlider />
     </>
   )
 }
